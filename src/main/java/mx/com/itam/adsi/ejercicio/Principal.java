58ac176648e77167e90319de8a3eb4c696c013d3
garellano <garellano@localhost>
2018-03-26 22:23:30 -0600

    Se agrega infraestructura de desarrollo
    Se crean los directorios de recursos para main y para test.
    Se agrega al pom.xml la dependencia de log4j.
    Se crea un esqueleto de clase de prueba.
    Se agrega una pruba simple de ejemplo.

--- src/main/java/mx/com/itam/adsi/ejercicio/Principal.java
+++ src/main/java/mx/com/itam/adsi/ejercicio/Principal.java
@@ -1,8 +1,12 @@
 package mx.com.itam.adsi.ejercicio;
 
+import org.apache.log4j.Logger;
+
 public class Principal{
+  private final static Logger LOG = Logger.getLogger(Principal.class);
+
   public static void main(String...argv) {
-    System.out.println(toArabic("fff1","0123456789abcdef"));
+    LOG.info("Conversión de fff1: "+toArabic("fff1","0123456789abcdef"));
   }
   private static int toArabic(String source, String alphabet) {
     int res = 0;
---
+++ src/test/java/mx/com/itam/adsi/ejercicio/TestPrincipal.java
@@ -0,0 +1,4 @@
+package mx.com.itam.adsi.ejercicio;
+
+public class TestPrincipal{
+}
